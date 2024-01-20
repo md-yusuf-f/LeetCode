ebfd37f32c3259146b3f700159e35782cb830e67
md-yusuf-f <mohammedyusuf1799@gmail.com>
2024-01-20 18:09:54 +0530

    Testing

--- src/solutions/MergeSortedArray.java
+++ src/solutions/MergeSortedArray.java
@@ -12,7 +12,7 @@ public class MergeSortedArray {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int[] mergedArray = Arrays.copyOf(nums1, m + n);
 
-        int i = 0;
+        int i = 1;
         int j = 0;
         int k = 0;
 
